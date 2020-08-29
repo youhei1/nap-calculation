be7828831c679d2c3241d1949c15a60d659b6fbd
youhei1 <w2cfa78972yq2bh@gmail.com>
2020-08-29 21:53:42 +0900

    画像2枚でいいらしい

--- index.php
+++ index.php
@@ -17,43 +17,7 @@
     <meta name="msapplication-TileColor" content="#0078d7">
     <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/common/img/favicons/favicon.ico">
     <link rel="icon" type="image/vnd.microsoft.icon" href="/common/img/favicons/favicon.ico">
-    <link rel="apple-touch-icon" sizes="57x57" href="/common/img/favicons/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/common/img/favicons/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/common/img/favicons/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/common/img/favicons/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/common/img/favicons/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/common/img/favicons/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/common/img/favicons/apple-touch-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="152x152" href="/common/img/favicons/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/common/img/favicons/apple-touch-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="36x36" href="/common/img/favicons/android-chrome-36x36.png">
-    <link rel="icon" type="image/png" sizes="48x48" href="/common/img/favicons/android-chrome-48x48.png">
-    <link rel="icon" type="image/png" sizes="72x72" href="/common/img/favicons/android-chrome-72x72.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/common/img/favicons/android-chrome-96x96.png">
-    <link rel="icon" type="image/png" sizes="128x128" href="/common/img/favicons/android-chrome-128x128.png">
-    <link rel="icon" type="image/png" sizes="144x144" href="/common/img/favicons/android-chrome-144x144.png">
-    <link rel="icon" type="image/png" sizes="152x152" href="/common/img/favicons/android-chrome-152x152.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="/common/img/favicons/android-chrome-192x192.png">
-    <link rel="icon" type="image/png" sizes="256x256" href="/common/img/favicons/android-chrome-256x256.png">
-    <link rel="icon" type="image/png" sizes="384x384" href="/common/img/favicons/android-chrome-384x384.png">
-    <link rel="icon" type="image/png" sizes="512x512" href="/common/img/favicons/android-chrome-512x512.png">
-    <link rel="icon" type="image/png" sizes="36x36" href="/common/img/favicons/icon-36x36.png">
-    <link rel="icon" type="image/png" sizes="48x48" href="/common/img/favicons/icon-48x48.png">
-    <link rel="icon" type="image/png" sizes="72x72" href="/common/img/favicons/icon-72x72.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="/common/img/favicons/icon-96x96.png">
-    <link rel="icon" type="image/png" sizes="128x128" href="/common/img/favicons/icon-128x128.png">
-    <link rel="icon" type="image/png" sizes="144x144" href="/common/img/favicons/icon-144x144.png">
-    <link rel="icon" type="image/png" sizes="152x152" href="/common/img/favicons/icon-152x152.png">
-    <link rel="icon" type="image/png" sizes="160x160" href="/common/img/favicons/icon-160x160.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="/common/img/favicons/icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="196x196" href="/common/img/favicons/icon-196x196.png">
-    <link rel="icon" type="image/png" sizes="256x256" href="/common/img/favicons/icon-256x256.png">
-    <link rel="icon" type="image/png" sizes="384x384" href="/common/img/favicons/icon-384x384.png">
-    <link rel="icon" type="image/png" sizes="512x512" href="/common/img/favicons/icon-512x512.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/common/img/favicons/icon-16x16.png">
-    <link rel="icon" type="image/png" sizes="24x24" href="/common/img/favicons/icon-24x24.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/common/img/favicons/icon-32x32.png">
-    <link rel="manifest" href="/common/img/favicons/manifest.json">
     <title>仮眠計算</title>
     <link rel="stylesheet" href="/common/css/lib/destyle.css"/>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&amp;display=swap"/>
